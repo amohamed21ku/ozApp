6acf9854bddeeccb99c52e51aa395ec9e34d971c
ABDELRAHMAN MOHAMED <amohamed21@ku.edu.tr>
2024-09-04 19:25:43 +0300

    edit

--- lib/components.dart
+++ lib/components.dart
@@ -1,14 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class RoundedButton extends StatelessWidget {
   const RoundedButton(
-      {super.key, required this.colour,
-        required this.title,
-        required this.onPressed,
-
-        required this.icon});
+      {super.key,
+      required this.colour,
+      required this.title,
+      required this.onPressed,
+      required this.icon});
 
   final Color colour;
   final String title;
@@ -18,12 +18,15 @@ class RoundedButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
       child: Material(
         elevation: 10.0,
         color: colour,
-        borderRadius: BorderRadius.circular(7.0),
+        borderRadius: BorderRadius.circular(8.0),
         child: MaterialButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
           onPressed: onPressed,
           minWidth: 325,
           height: 60,
@@ -57,7 +60,11 @@ class MY_textField extends StatelessWidget {
   final String hintText;
   var onchange;
   final double h;
-  MY_textField({super.key, required this.hintText , required this.onchange, required this.h});
+  MY_textField(
+      {super.key,
+      required this.hintText,
+      required this.onchange,
+      required this.h});
 
   @override
   Widget build(BuildContext context) {
@@ -65,13 +72,13 @@ class MY_textField extends StatelessWidget {
       width: double.infinity,
       height: h,
       decoration: BoxDecoration(
-          color: const Color(0xff4E4B4A), borderRadius: BorderRadius.circular(10)),
+          color: const Color(0xff4E4B4A),
+          borderRadius: BorderRadius.circular(10)),
       child: TextField(
         style: const TextStyle(color: Colors.white),
-
         onChanged: onchange,
         decoration:
-        InputDecoration(border: InputBorder.none, hintText: hintText),
+            InputDecoration(border: InputBorder.none, hintText: hintText),
         textAlign: TextAlign.center,
       ),
     );
@@ -81,7 +88,8 @@ class MY_textField extends StatelessWidget {
 // -----------------------------------
 
 class RoundedButton2 extends StatelessWidget {
-  const RoundedButton2({super.key,
+  const RoundedButton2({
+    super.key,
     required this.colour,
     required this.title,
     required this.onPressed,
@@ -178,13 +186,12 @@ class myiconbutton extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
   final Color color;
-  const  myiconbutton(
-      {super.key, required this.icon,
-        required this.title,
-        required this.onPressed,
-        required this.color
-      });
-
+  const myiconbutton(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.onPressed,
+      required this.color});
 
   @override
   Widget build(BuildContext context) {
@@ -206,21 +213,19 @@ class myiconbutton extends StatelessWidget {
   }
 }
 
-
 // =================================
 
 class RoundedButtonSmall extends StatelessWidget {
-  const RoundedButtonSmall({super.key,
-    required this.colour,
-    required this.title,
-    required this.onPressed,
-    required this.width,
-    required this.height,
-    required this.icon,
-    required this.iconColor,
-    required this.textcolor
-
-  });
+  const RoundedButtonSmall(
+      {super.key,
+      required this.colour,
+      required this.title,
+      required this.onPressed,
+      required this.width,
+      required this.height,
+      required this.icon,
+      required this.iconColor,
+      required this.textcolor});
 
   final Color colour;
   final String title;
@@ -233,51 +238,52 @@ class RoundedButtonSmall extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 8.0,
+    return MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
       color: colour,
-      borderRadius: BorderRadius.circular(12.0),
-      child: MaterialButton(
-        onPressed: onPressed,
-        minWidth: width,
-        height: height,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon,color: iconColor,),
-            const SizedBox(width: 10,),
-            Text(
-              title,
-              style: GoogleFonts.poppins(
-                  color: textcolor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              width: 7,
-            ),
-          ],
-        ),
+      elevation: 5,
+      onPressed: onPressed,
+      minWidth: width,
+      height: height,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: iconColor,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            title,
+            style: GoogleFonts.poppins(
+                color: textcolor, fontSize: 14, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+        ],
       ),
     );
   }
 }
 
-
 //=====================================================================
 
 class RoundedButtonSmall_Sharb extends StatelessWidget {
-  const RoundedButtonSmall_Sharb({super.key,
-    required this.colour,
-    required this.title,
-    required this.onPressed,
-    required this.width,
-    required this.height,
-    required this.icon,
-    required this.iconColor,
-    required this.textcolor
-
-  });
+  const RoundedButtonSmall_Sharb(
+      {super.key,
+      required this.colour,
+      required this.title,
+      required this.onPressed,
+      required this.width,
+      required this.height,
+      required this.icon,
+      required this.iconColor,
+      required this.textcolor});
 
   final Color colour;
   final String title;
@@ -302,8 +308,13 @@ class RoundedButtonSmall_Sharb extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(icon,color: iconColor,),
-              const SizedBox(width: 10,),
+              Icon(
+                icon,
+                color: iconColor,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
               Text(
                 title,
                 style: GoogleFonts.poppins(
@@ -355,4 +366,3 @@ class MyTextField extends StatelessWidget {
     );
   }
 }
-
--- lib/constants.dart
+++ lib/constants.dart
@@ -3,18 +3,16 @@ import 'package:flutter/material.dart';
 const kTextfielDecoration = InputDecoration(
   fillColor: Colors.white,
   filled: true,
-  hintText: 'Enter your email',
-  hintStyle: TextStyle(color: Colors.grey),
-  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(7.0)),
   ),
   enabledBorder: OutlineInputBorder(
     borderSide: BorderSide(color: Colors.grey, width: 1.0),
-    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+    borderRadius: BorderRadius.all(Radius.circular(8.0)),
   ),
   focusedBorder: OutlineInputBorder(
     borderSide: BorderSide(color: Color(0xffa4392f), width: 1.5),
-    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    borderRadius: BorderRadius.all(Radius.circular(16.0)),
   ),
 );
